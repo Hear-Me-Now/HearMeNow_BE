76de59f7b2293690818d1da415651316e1a0d79b
Joe King <j.king92@gmail.com>
2023-03-28 15:43:01 -0700

    test: Add service test expectations

--- spec/services/sound_service_spec.rb
+++ spec/services/sound_service_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe SoundService do
   describe 'self.get_sound_data' do
     it 'returns a json containing sound information', :vcr do
-      data = SoundService.get_sound_data(658349)
-      binding.pry
+      id = 658349
+      data = SoundService.get_sound_data(id)
+      
+      expect(data[:id]).to eq(id)
+      expect(data[:previews][:"preview-hq-mp3"]).to eq("https://cdn.freesound.org/previews/658/658349_12274174-hq.mp3")
     end
   end
 end
