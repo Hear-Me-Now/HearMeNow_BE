999a30a8ce706fd6f2432d472c8a7e8586420c23
Samuel Cox <samc1253@gmail.com>
2023-03-30 14:23:33 -0600

    Titleize correct answers

--- app/models/sound_card.rb
+++ app/models/sound_card.rb
@@ -2,4 +2,10 @@ class SoundCard < ApplicationRecord
   has_many :wrong_answers, dependent: :destroy
 
   validates_presence_of :correct_answer, :category
+
+  before_validation :capitalize_answer
+
+  def capitalize_answer
+    self.correct_answer = self.correct_answer.titleize
+  end
 end
