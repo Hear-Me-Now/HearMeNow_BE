2d31d05006cf5e5bbd085da129a1ed90e55579f1
Samuel Cox <samc1253@gmail.com>
2023-03-30 14:22:32 -0600

    Adjust wrong answers to be titleized

--- app/models/wrong_answer.rb
+++ app/models/wrong_answer.rb
@@ -2,4 +2,10 @@ class WrongAnswer < ApplicationRecord
   belongs_to :sound_card
 
   validates_presence_of :answer
+
+  before_validation :capitalize_answer
+
+  def capitalize_answer
+    self.answer = self.answer.titleize
+  end
 end
