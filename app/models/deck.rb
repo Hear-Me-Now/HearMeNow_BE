9bcaeb74f194f7bcb58c66ab93cf6dceeacbd920
Ryan Canton <ryancanton999@gmail.com>
2023-03-31 15:52:46 -0600

    Feat: Create mutations for decks and model methods to make deck_card models

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,20 @@
 class Deck < ApplicationRecord
   validates_presence_of :category
+  after_create :get_cards
+
+  private
+  
+  def get_cards
+    sound_cards.each do |sc|
+      DeckCard.create(sound_card_id: sc.id, deck_id: self.id)
+    end
+  end
+
+  def sound_cards
+    unless category.downcase.include?('misc')
+      SoundCard.where("lower(category) = ?", category.downcase).shuffle.shuffle.first(8)
+    else
+      SoundCard.all.shuffle.shuffle.first(8)
+    end
+  end
 end
