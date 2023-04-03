5addfd4061c64200041e6ef2fdbc7c007560213a
Joe King <j.king92@gmail.com>
2023-04-02 18:51:07 -0700

    test: sad path tests for sound card query

--- spec/requests/sound_card_query_spec.rb
+++ spec/requests/sound_card_query_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'Sound Card Requests', type: :request do
   describe 'Sound Card Queries' do
     it 'returns asound card in the correct category for the given deck id', :vcr do
       deck_id = Deck.create!(category: "Animals").id
-
-      sound_card = query_sound_cards_by_deck_id(deck_id)
+      response = query_sound_cards_by_deck_id(deck_id)
+      sound_card = response.dig('data', 'soundCard')
 
       expect(sound_card['category']).to eq('Animals')
       expect(SoundCard.exists?(sound_card['correctAnswer']))
@@ -20,14 +20,24 @@ RSpec.describe 'Sound Card Requests', type: :request do
 
   describe 'Sound Card Sad Paths' do
     it 'returns an error if deck is out of cards', :vcr do
-      # TODO: Write test
+      deck_id = Deck.create!(category: "Animals").id
+      8.times do 
+        query_sound_cards_by_deck_id(deck_id)
+      end
+      response = query_sound_cards_by_deck_id(deck_id)
+      errors = response.dig('errors')
+      
+      expect(errors.count).to eq(1)
+      expect(errors.first["message"]).to eq("Cannot return null for non-nullable field Query.soundCard")
     end
 
     it 'returns an error if given deck id does not exist', :vcr do
+      Deck.create!(category: "Misc")
+      deck_id = Deck.maximum(:id) + 1
 
-      # errors = response.dig('errors')
-
-      # expect(errors.first['message']).to eq("Field 'soundCardsByCategory' is missing required arguments: category")
+      response = query_sound_cards_by_deck_id(deck_id)
+      errors = response.dig('errors')
+      expect(errors.first['message']).to eq("SoundCard not found")
     end
   end
 
@@ -46,6 +56,6 @@ RSpec.describe 'Sound Card Requests', type: :request do
       }
     GQL
 
-    response.dig('data', 'soundCard')
+    # response.dig('data')
   end
 end
