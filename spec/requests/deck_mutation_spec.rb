3233a39d275fb32fd0ae1e88466c7fc4e08a1451
Joe King <j.king92@gmail.com>
2023-04-02 19:06:05 -0700

    test: Add edge case and sad path tests for deck mutation

--- spec/requests/deck_mutation_spec.rb
+++ spec/requests/deck_mutation_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'deck mutation', type: :request do
   it 'adds a deck entry to the database' do
-    mutation_response = decks_mutation
+    mutation_response = decks_mutation('Animals')
     deck = mutation_response["createDeck"]["deck"]
     errors = mutation_response["createDeck"]["errors"]
     new_deck = Deck.last
@@ -15,26 +15,51 @@ RSpec.describe 'deck mutation', type: :request do
 
   describe 'deck edge cases' do
     it 'returns a deck with correct categories case insensitively' do
-      # TODO: write test
+      mutation_response = decks_mutation('iNsTrumENTS')
+      deck = mutation_response["createDeck"]["deck"]
+      new_deck = Deck.last
+  
+      expect(new_deck.category).to eq("iNsTrumENTS")
+      new_deck.sound_cards.each do |sc|
+        expect(sc.category).to eq('Instruments')
+      end
     end
   end
 
   describe 'deck sad paths' do
-    it 'returns an error if category does not exist' do
-      # TODO: Write test
+    it 'returns a deck with no cards if category does not exist' do
+      mutation_response = decks_mutation('accents')
+      deck = mutation_response["createDeck"]["deck"]
+      new_deck = Deck.last
+  
+      expect(new_deck.sound_cards.count).to eq(0)
     end
 
     it 'returns an error if no category is given' do
-      # TODO: Write test
+      response = gql <<-GQL 
+        mutation createDeck{
+          createDeck(input: {
+          }) {
+            deck {
+              id
+            }
+            errors
+          }
+        }
+      GQL
+      errors = response.dig('errors')
+
+      expect(errors.first['message']).to eq("Argument 'category' on InputObject 'CreateDeckInput' is required. Expected type String!")
+      expect(Deck.last).to be nil
     end
   end
   private
 
-  def decks_mutation()
+  def decks_mutation(category)
     response = gql <<-GQL
       mutation createDeck{
         createDeck(input: {
-          category: "Animals"
+          category: "#{category}"
         }) {
           deck {
             id
