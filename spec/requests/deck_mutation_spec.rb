ac26aa933c359bb3f676c5d1161f9aafa241c297
Joe King <j.king92@gmail.com>
2023-04-02 18:32:30 -0700

    test: Outline tests to write and initial happy path test for sound cary by deck id query

--- spec/requests/deck_mutation_spec.rb
+++ spec/requests/deck_mutation_spec.rb
@@ -10,8 +10,24 @@ RSpec.describe 'deck mutation', type: :request do
     expect(new_deck.category).to eq("Animals")
     expect(deck["id"]).to eq(new_deck.id.to_s)
     expect(errors).to eq([])
+    expect(new_deck.sound_cards.count).to eq(8)
   end
 
+  describe 'deck edge cases' do
+    it 'returns a deck with correct categories case insensitively' do
+      # TODO: write test
+    end
+  end
+
+  describe 'deck sad paths' do
+    it 'returns an error if category does not exist' do
+      # TODO: Write test
+    end
+
+    it 'returns an error if no category is given' do
+      # TODO: Write test
+    end
+  end
   private
 
   def decks_mutation()
--- spec/requests/sound_card_query_spec.rb
+++ spec/requests/sound_card_query_spec.rb
@@ -4,89 +4,39 @@ RSpec.describe 'Sound Card Requests', type: :request do
   before :each do
     allow(SoundFacade).to receive(:get_link).and_return("https://cdn.freesound.org/previews/510/510917_11157357-hq.mp3")
   end
+
   describe 'Sound Card Queries' do
-    it 'returns an array of sound cards based on a category', :vcr do
-      data = query_sound_cards_by_category('Animals', 8)
+    it 'returns asound card in the correct category for the given deck id', :vcr do
+      deck_id = Deck.create!(category: "Animals").id
 
-      data.each do |sound_card|
-        expect(sound_card['category']).to eq('Animals')
-        expect(SoundCard.exists?(sound_card['correctAnswer']))
-        expect(SoundCard.exists?(sound_card['id']))
-        expect(SoundCard.find(sound_card['id']).wrong_answers.pluck(:answer).sort).to eq(sound_card['wrongAnswers'].sort)
-      end
-    end
-  end
+      sound_card = query_sound_cards_by_deck_id(deck_id)
 
-  describe 'Sound Card Edge Cases' do
-    it 'returns sound cards by category case insensitively', :vcr do
-      data = query_sound_cards_by_category('inStRumEnts', 8)
-
-      data.each do |sound_card|
-        expect(sound_card['category']).to eq('Instruments')
-        expect(SoundCard.exists?(sound_card['correctAnswer']))
-        expect(SoundCard.exists?(sound_card['id']))
-        expect(SoundCard.find(sound_card['id']).wrong_answers.pluck(:answer).sort).to eq(sound_card['wrongAnswers'].sort)
-      end
-    end
-
-    it 'sets the limit to 24 if too high a limit is given', :vcr do
-      data = query_sound_cards_by_category('inStRumEnts', 283)
-
-      expect(data.length).to eq(24)
-    end
-
-
-    it 'returns 8 sound cards if no limit is given', :vcr do
-      response = gql <<-GQL
-        query soundCardsByCategoryQuerySpec {
-          soundCardsByCategory(category: "instruments") {
-            id
-            category
-            correctAnswer
-            link
-            wrongAnswers
-          }
-        }
-      GQL
-
-      data = response.dig('data', 'soundCardsByCategory')
-
-      expect(data.length).to eq(8)
+      expect(sound_card['category']).to eq('Animals')
+      expect(SoundCard.exists?(sound_card['correctAnswer']))
+      expect(SoundCard.exists?(sound_card['id']))
+      expect(SoundCard.find(sound_card['id']).wrong_answers.pluck(:answer).sort).to eq(sound_card['wrongAnswers'].sort)
     end
   end
 
   describe 'Sound Card Sad Paths' do
-    it 'returns an empty array if a nonexistent category is given', :vcr do
-      data = query_sound_cards_by_category('people', 8)
-
-      expect(data).to eq([])
+    it 'returns an error if deck is out of cards', :vcr do
+      # TODO: Write test
     end
 
-    it 'returns an error if no category is given', :vcr do
-      response = gql <<-GQL
-        query soundCardsByCategoryQuerySpec {
-          soundCardsByCategory(limit: 8) {
-            id
-            category
-            correctAnswer
-            link
-            wrongAnswers
-          }
-        }
-      GQL
+    it 'returns an error if given deck id does not exist', :vcr do
 
-      errors = response.dig('errors')
+      # errors = response.dig('errors')
 
-      expect(errors.first['message']).to eq("Field 'soundCardsByCategory' is missing required arguments: category")
+      # expect(errors.first['message']).to eq("Field 'soundCardsByCategory' is missing required arguments: category")
     end
   end
 
   private
 
-  def query_sound_cards_by_category(category, limit)
+  def query_sound_cards_by_deck_id(deck_id)
     response = gql <<-GQL
-      query soundCardsByCategoryQuerySpec {
-        soundCardsByCategory(category: "#{category}", limit: #{limit}) {
+      query soundCardsByDeckIdQuerySpec {
+        soundCard(deckId: "#{deck_id}") {
           id
           category
           correctAnswer
@@ -96,6 +46,6 @@ RSpec.describe 'Sound Card Requests', type: :request do
       }
     GQL
 
-    response.dig('data', 'soundCardsByCategory')
+    response.dig('data', 'soundCard')
   end
 end
