9b1457d97160d93a57be82f5e4da0cbd41ad593c
Samuel Cox <samc1253@gmail.com>
2023-04-04 11:58:58 -0600

    Test: Add testing for Sheet Facade

--- spec/facades/sheet_facade_spec.rb
+++ spec/facades/sheet_facade_spec.rb
@@ -3,7 +3,23 @@ require 'rails_helper'
 RSpec.describe SheetFacade do
   describe 'getting sheets' do
     it 'returns cell information for sound card sheet', :vcr do
+      sound_card_data = SheetFacade.get_sheets('Sound%20Cards')
 
+      expect(sound_card_data.first.second).to eq('4202')
+      expect(sound_card_data[44].second).to eq('256568')
+      expect(sound_card_data.first.first).to eq('Misc')
+      expect(sound_card_data[34].first).to eq('Machines')
+      expect(sound_card_data.first.third).to eq('Boiling Water')
+      expect(sound_card_data[46].third).to eq('Howler Monkey')
+    end
+
+    it 'returns cell information for wrong answer sheet', :vcr do
+      wrong_answer_data = SheetFacade.get_sheets('Wrong%20Answers')
+
+      expect(wrong_answer_data.first.first).to eq('4202')
+      expect(wrong_answer_data[44].second).to eq('Timpani')
+      expect(wrong_answer_data.first.second).to eq('Hot Tub Jets')
+      expect(wrong_answer_data[34].first).to eq('58811')
     end
   end
 end
