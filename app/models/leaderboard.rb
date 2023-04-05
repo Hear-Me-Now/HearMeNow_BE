18afaf458fb2f81e03ed00c13409e17afdb1fa6d
Samuel Cox <samc1253@gmail.com>
2023-04-05 10:36:02 -0600

    Add difficulty back in

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Leaderboard < ApplicationRecord
-  validates_presence_of :name, :score
+  validates_presence_of :name, :score, :difficulty
   validates_numericality_of :score, greater_than_or_equal_to: 0
 end
--- spec/models/leaderboard_spec.rb
+++ spec/models/leaderboard_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Leaderboard do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :score }
+    it { should validate_presence_of :difficulty }
     it { should validate_numericality_of(:score).is_greater_than_or_equal_to(0) }
   end
 end
